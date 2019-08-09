30b8cbfab4290ac5a56f90c22825b913144551f3
Sidas Snieska <ssnieskiokas@gmail.com>
2019-08-09 14:35:00 +0300

    0.1.2 New abstraction for ACF Blade. Passing all fields as vars, more handy.

---
+++ src/AbstractACFBladeBlock.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prophe1\ACFBlockz;
+
+use function App\template;
+
+/**
+ * Class AbstractBladeBlock
+ *
+ * @package Itineris\AcfGutenblocks
+ */
+abstract class AbstractACFBladeBlock extends AbstractBladeBlock
+{
+    /**
+     * Block acf parameters
+     *
+     * @var array
+     */
+    protected $acf;
+
+    /**
+     * We take all block fields and make them available as variables
+     *
+     * @param array $block
+     */
+    public function renderBlockCallback(array $block): void
+    {
+        $frontend = apply_filters(
+            'acfblocks/render_block_frontend_path',
+            "$this->dir/blocks/{$this->getName()}",
+            $this
+        );
+
+        $this->acf = get_fields();
+
+        // Overwrite controller key if this name was taken
+        $this->acf['controller'] = $this;
+
+        echo template($frontend, $this->acf);
+    }
+}
